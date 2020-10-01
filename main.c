b4b854528e1d424898fbd758813439ee4a18d19d
blubrom <61847724+blubrom@users.noreply.github.com>
2020-10-02 00:55:42 +0200

    Add files via upload
    Add functions for a functionnal 2 player game (no end condition for now) + minor changes to allow the code to run

--- main.c
+++ main.c
@@ -1,26 +1,21 @@
 /* Template of the 7 wonders of the world of the 7 colors assigment. */
 
-#include <stdio.h> /* Import the system's printf */
+#include <stdio.h>     /* Import the system's printf */
+#include "board.h"     /* Import our board module */
+#include <time.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <time.h>
-#include "queue.h"
-#include "board.h" /* Import our board module */
 
 /** Program entry point */
 int main(void)
 {
     srand((unsigned)time(NULL));
-    printf("\n\nWelcome to the 7 wonders of the world of the 7 colors\n"
-           "*****************************************************\n\n"
-           "Current board state:\n");
     init_board();
-    print_board();
-    char c;
-    printf("quelle couleur voulez vous jouer ?");
-    scanf("%c", &c);
-    bad_update_board('1', c);
-    print_board();
+    printf("\n\nWelcome to the 7 wonders of the world of the 7 colors\n"
+	   "*****************************************************\n\n"
+	   "Current board state:\n");
+     int winner = run_game();
+     print_end_screen(winner);
 
     return 0; // Everything went well
 }
